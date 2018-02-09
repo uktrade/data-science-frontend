5b3bd620c68af6dd0c9e2c246779adfbcbf26cf4
Graham Veal <grahamveal@gmail.com>
2018-02-09 12:26:42 +0000

    Fix error page

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,8 +1,9 @@
+const homeController = require( './controllers/home' );
 const ssoController = require( './controllers/sso' );
 
 module.exports = function( express, app ){
 
-	app.get( '/', ( req, res ) => res.render( 'index' ) );
+	app.get( '/', homeController );
 	app.get( '/login/', ssoController.authRedirect );
 	app.get( '/login/callback/', ssoController.callback );
 };
