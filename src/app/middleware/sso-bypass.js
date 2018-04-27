21229cddb20f9405d163467940d56b9335a120bd
Graham Veal <grahamveal@gmail.com>
2018-04-27 10:55:43 +0100

    Add .npmrc file and log when bypassing SSO

--- src/app/middleware/sso-bypass.js
+++ src/app/middleware/sso-bypass.js
@@ -1,9 +1,11 @@
 const config = require( '../config' );
+const logger = require( '../lib/logger' );
 
 module.exports = function( req, res, next ){
 
 	if( config.sso.bypass ){
 
+		logger.debug( 'Bypassing SSO' );
 		req.session.ssoToken = 'ssobypass';
 	}
 
