d7bda9340c6926029f0fd5bd51d49df2b28f3a54
John Fieldman <hello@johnfieldman.com>
2018-10-01 16:53:46 +0100

    Format template dates and long numbers
    Create template filter that adds a comma every thousand for long numbers
    Add date formating as template filter.
    Update es lint settings to not signal semi colons.
    Refactor routes and acs to be functional rather than object oriented.

--- src/app/lib/nunjucks-filters/index.js
+++ src/app/lib/nunjucks-filters/index.js
@@ -1,4 +1,23 @@
+const dateFns = require('date-fns')
+
+function formatNumberWithCommas (x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+function formatDate (value, format = 'MMMM YYYY') {
+  if (!value) {
+    return value
+  }
+  const parsedDate = dateFns.parse(value)
+
+  if (!dateFns.isValid(parsedDate)) { return value }
+  return dateFns.format(parsedDate, format)
+}
+
+
 module.exports = function( env ){
-	env.addFilter( 'dateOnly', require( './date-only' ) );
-	env.addFilter( 'isArray', require( 'lodash' ) );
+	env.addFilter( 'formatDate', formatDate )
+	env.addFilter( 'formatNumberWithCommas', formatNumberWithCommas )
+	env.addFilter( 'dateOnly', require( './date-only' ) )
+	env.addFilter( 'isArray', require( 'lodash' ) )
 };
