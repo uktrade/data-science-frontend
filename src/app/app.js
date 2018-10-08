71218503e2f8bdfe9487295b49a6674b68a71b1f
John Fieldman <hello@johnfieldman.com>
2018-10-08 14:30:11 +0100

    DST-1068 changed gov templates paths config to also recognize prod

--- src/app/app.js
+++ src/app/app.js
@@ -28,6 +28,7 @@ module.exports = {
     const pathToPublic = path.resolve(__dirname, '../public')
     const staticMaxAge = (isDev ? 0 : '2y')
     const nunjucksEnv = nunjucks.configure([
+      path.resolve(__dirname, (isDev ? '../' : '') + '../node_modules/govuk-frontend'),
       'node_modules/govuk-frontend/components',
       `${__dirname}/views`,
       `${__dirname}/components`,
