e1a6902e185a422d9dbecc64d83fd92ebf201ccd
John Fieldman <hello@johnfieldman.com>
2018-10-08 20:02:07 +0100

    DST-1075 cleaned up CSP errors and removed legacy scripts from client

--- src/app/middleware/headers.js
+++ src/app/middleware/headers.js
@@ -1,28 +1,27 @@
 const cspValues = [
-	`default-src 'none'`,
-	`base-uri 'self'`,
-	`script-src 'self' 'unsafe-inline' www.google-analytics.com maps.googleapis.com`,
-	`style-src 'self' 'unsafe-inline' fonts.googleapis.com`,
-	`font-src 'self' fonts.gstatic.com`,
-	`img-src 'self' www.google-analytics.com maps.gstatic.com maps.googleapis.com`,
-	`form-action 'self'`,
-	`connect-src 'self'`
-].join( ';' );
+  `default-src 'none'`,
+  `base-uri 'self'`,
+  `script-src 'self' 'unsafe-inline' www.google-analytics.com maps.googleapis.com cdnjs.cloudflare.com`,
+  `style-src 'self' 'unsafe-inline' fonts.googleapis.com cdnjs.cloudflare.com`,
+  `font-src 'self' fonts.gstatic.com`,
+  `img-src 'self' www.google-analytics.com maps.gstatic.com maps.googleapis.com`,
+  `form-action 'self'`,
+  `connect-src 'self'`,
+].join('')
 
-module.exports = function( isDev ){
+module.exports = function (isDev) {
+  return function (req, res, next) {
+    res.setHeader('X-Download-Options', 'noopen')
+    res.setHeader('X-XSS-Protection', '1 mode=block')
+    res.setHeader('X-Content-Type-Options', 'nosniff')
+    res.setHeader('X-Frame-Options', 'deny')
+    res.setHeader('Content-Security-Policy', cspValues)
+    res.setHeader('Cache-Control', 'no-cache, no-store')
 
-	return function( req, res, next ){
-		res.setHeader( 'X-Download-Options', 'noopen' );
-		res.setHeader( 'X-XSS-Protection', '1; mode=block' );
-		res.setHeader( 'X-Content-Type-Options', 'nosniff' );
-		res.setHeader( 'X-Frame-Options', 'deny' );
-		res.setHeader( 'Content-Security-Policy', cspValues );
-		res.setHeader( 'Cache-Control', 'no-cache, no-store' );
+    if (!isDev) {
+      res.setHeader('Strict-Transport-Security', 'max-age=31536000 includeSubDomains')
+    }
 
-		if( !isDev ){
-			res.setHeader( 'Strict-Transport-Security', 'max-age=31536000; includeSubDomains' );
-		}
-
-		next();
-	};
-};
+    next()
+  }
+}
