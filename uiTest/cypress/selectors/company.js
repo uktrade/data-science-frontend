e5b74d79e455cf9d7b6e4f20ff78d85dd9bf09b2
Pippo Raimondi <filipporai@gmail.com>
2018-10-09 12:16:12 +0100

    Initial cypress test suite

---
+++ uiTest/cypress/selectors/company.js
@@ -0,0 +1,7 @@
+const company = {
+    companyList: '.govuk-list.govuk-body',
+    companyContent: '.govuk-list.govuk-body > li',
+    dataHub: '.govuk-list.govuk-body > li:first-child > div > a'
+};
+
+export default company;
