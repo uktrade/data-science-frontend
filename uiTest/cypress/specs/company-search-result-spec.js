ff9b045d7f835a2c87ccb32f0e2dccc28258708e
Pippo Raimondi <filipporai@gmail.com>
2018-12-12 12:40:06 +0000

    ensuring tests wait for xhr request to be complete

--- uiTest/cypress/specs/company-search-result-spec.js
+++ uiTest/cypress/specs/company-search-result-spec.js
@@ -3,11 +3,13 @@ import selectors from '../selectors';
 describe('Company search result', function() {
     beforeEach(() => {
         cy.server();
+        cy.route('/?sort*').as('filterResults');
         cy.visit('/');
     });
 
     it('displays empty company result page', () => {
         cy.get(selectors.filters.companyName).type('invalid company').type('{enter}');
+        cy.wait('@filterResults');
 
         cy.get(selectors.company.companyEmptyContent).should('contain',
             'Oh no, there are no results for your search');
@@ -15,12 +17,14 @@ describe('Company search result', function() {
 
     it('displays a single company result page', () => {
         cy.get(selectors.filters.companyName).type('single company').type('{enter}');
+        cy.wait('@filterResults');
 
         cy.get(selectors.company.companyList).children().should('have.length', 1);
     });
 
     it('displays multiple company result page', () => {
         cy.get(selectors.filters.companyName).type('multiple company').type('{enter}');
+        cy.wait('@filterResults');
 
         cy.get(selectors.company.companyList).children().should('have.length', 20);
     });
