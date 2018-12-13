f0edade0c640464f1f66137aeb1d101649df3cf6
Pippo Raimondi <filipporai@gmail.com>
2018-12-13 14:46:58 +0000

    adding more functional tests for filters

--- uiTest/cypress/selectors/filters.js
+++ uiTest/cypress/selectors/filters.js
@@ -2,6 +2,11 @@ const filters = {
     companyName: '#company-name',
     exportPotential: '#export-potential-',
     commodityCode: '#commodity-code',
+    exportEvidenceStartDate: '#export-evidence-start-date',
+    exportEvidenceEndDate: '#export-evidence-end-date',
+    sicCodes: '#sic-codes',
+    turnOverMin: '#turnover-minimum',
+    turnOverMax: '#turnover-maximum',
 };
 
 export default filters;
--- uiTest/cypress/specs/company-filter-spec.js
+++ uiTest/cypress/specs/company-filter-spec.js
@@ -5,6 +5,7 @@ describe('Company filters', () => {
         cy.server();
         cy.route('/?sort*').as('filterResults');
         cy.visit('/');
+        cy.get(selectors.company.companyList).children().should('have.length', 20);
     });
 
     describe('Export Potential', () => {
@@ -18,9 +19,10 @@ describe('Company filters', () => {
                 expect(xhr.url).to.contain('?sort=export_propensity:desc&export-potential=Low');
             });
     
-            cy.get(selectors.company.companyList).children().should('have.length', 20);
+            cy.get(selectors.company.companyList).children().should('have.length', 1);
         });
     });
+
     describe('Commodity code', () => {
         it('Filters by single commodity code', () => {
             cy.get(selectors.filters.commodityCode)
@@ -32,7 +34,138 @@ describe('Company filters', () => {
                 expect(xhr.url).to.contain('?sort=export_propensity:desc&export-codes=12345');
             });
     
-            cy.get(selectors.company.companyList).children().should('have.length', 20);
+            cy.get(selectors.company.companyList).children().should('have.length', 1);
         });
     })
+
+    // Disabled until story DST-1220 is fixed
+    xdescribe('Latest export evidence', () => {
+        it('Filters by start date only', () => {
+            cy.get(selectors.filters.exportEvidenceStartDate)
+                .should('not.be.disabled')
+                .type('2000-10')
+                .type('{enter}');
+
+            cy.wait('@filterResults').its('url').should('contain',
+                '?sort=export_propensity:desc&export-evidence-start-date=2000-10')
+    
+            cy.get(selectors.company.companyEmptyContent).should('contain',
+            'Oh no, there are no results for your search');
+        });
+
+        it('Filters by end date only', () => {
+            cy.get(selectors.filters.exportEvidenceEndDate)
+                .should('not.be.disabled')
+                .type('2000-10')
+                .type('{enter}');
+
+            cy.wait('@filterResults').its('url').should('contain',
+                '?sort=export_propensity:desc&export-evidence-end-date=2000-10')
+    
+            cy.get(selectors.company.companyEmptyContent).should('contain',
+            'Oh no, there are no results for your search');
+        });
+
+        it('Filters by start and end date', () => {
+            cy.get(selectors.filters.exportEvidenceStartDate)
+                .should('not.be.disabled')
+                .type('2000-10')
+                .type('{enter}');
+                
+            cy.wait('@filterResults').its('url').should('contain',
+                '?sort=export_propensity:desc&export-evidence-start-date=2000-10')
+
+            cy.get(selectors.filters.exportEvidenceEndDate)
+                .should('not.be.disabled')
+                .type('2018-10')
+                .type('{enter}');
+
+            cy.wait('@filterResults').its('url').should('contain',
+                '?sort=export_propensity:desc&export-evidence-start-date=2000-10&export-evidence-end-date=2018-10');
+    
+            cy.get(selectors.company.companyList).children().should('have.length', 1);
+        });
+
+        it('Handles an invalid date format', () => {
+            cy.get(selectors.filters.exportEvidenceStartDate)
+                .should('not.be.disabled')
+                .type('invalid')
+                .type('{enter}');
+    
+            cy.get(selectors.company.companyEmptyContent).should('contain',
+            'Oh no, there are no results for your search');
+        });
+    });
+
+    describe('Sic code', () => {
+        it('Filters by sic code', () => {
+            cy.get(selectors.filters.sicCodes)
+                .should('not.be.disabled')
+                .type('12345')
+                .type('{enter}');
+
+            cy.wait('@filterResults').then((xhr) => {
+                expect(xhr.url).to.contain('?sort=export_propensity:desc&sic-codes=12345');
+            });
+    
+            cy.get(selectors.company.companyList).children().should('have.length', 1);
+        });
+    });
+
+    describe('Turnover', () => {
+        it('Filters by minimum only', () => {
+            cy.get(selectors.filters.turnOverMin)
+                .should('not.be.disabled')
+                .type('100')
+                .type('{enter}');
+
+            cy.wait('@filterResults').its('url').should('contain',
+                '?sort=export_propensity:desc&turnover-minimum=100')
+    
+            cy.get(selectors.company.companyList).children().should('have.length', 1);
+        });
+
+        it('Filters by maximum only', () => {
+            cy.get(selectors.filters.turnOverMax)
+                .should('not.be.disabled')
+                .type('1')
+                .type('{enter}');
+
+            cy.wait('@filterResults').its('url').should('contain',
+                '?sort=export_propensity:desc&turnover-maximum=1')
+    
+            cy.get(selectors.company.companyEmptyContent).should('contain',
+            'Oh no, there are no results for your search');
+        });
+
+        it('Filters by minimum and maximum', () => {
+            cy.get(selectors.filters.turnOverMin)
+                .should('not.be.disabled')
+                .type('100')
+                .type('{enter}');
+                
+            cy.wait('@filterResults').its('url').should('contain',
+                '?sort=export_propensity:desc&turnover-minimum=100')
+
+            cy.get(selectors.filters.turnOverMax)
+                .should('not.be.disabled')
+                .type('1000')
+                .type('{enter}');
+
+            cy.wait('@filterResults').its('url').should('contain',
+                '?sort=export_propensity:desc&turnover-minimum=100&turnover-maximum=1000')
+    
+            cy.get(selectors.company.companyList).children().should('have.length', 1);
+        });
+
+        it('Handles non numeric values', () => {
+            cy.get(selectors.filters.turnOverMin)
+                .should('not.be.disabled')
+                .type('not4number')
+                .type('{enter}');
+    
+            cy.get(selectors.company.companyEmptyContent).should('contain',
+            'Oh no, there are no results for your search');
+        });
+    });
 });
