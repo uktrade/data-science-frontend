7895b28283418ec0b7ffdcbd0378f5e985a1ec40
Kristof Geebelen <geebelenk@gmail.com>
2019-04-11 17:12:31 +0100

    remove datahub and google links in search page

--- uiTest/selectors/company.js
+++ uiTest/selectors/company.js
@@ -2,7 +2,7 @@ const company = {
   companyList: '.govuk-list.govuk-body',
   companyContent: '.govuk-list.govuk-body > li',
   companyEmptyContent: 'article > ol',
-  dataHub: 'div.govuk-grid-column-full > a',
+  dataHub: 'h3.govuk-heading-m > a',
   companyNames: 'ol.govuk-list li h3',
   pagination: '.app-pagination-list-item > a',
 }
