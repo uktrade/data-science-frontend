6228a8332900d01c73bfc35c343cc6891b8d5543
John Fieldman <hello@johnfieldman.com>
2019-04-15 10:52:02 +0100

    fixup! DST-1304 updated back link to take the right parameters

--- src/app/controllers/company-timeline.js
+++ src/app/controllers/company-timeline.js
@@ -4,9 +4,8 @@ async function renderCompanyTimeline (req, res) {
   const companyId = req.params.company_id
   const companyProfile = await backendService.getCompanyProfileByInternalCompanyId(companyId)
   const profile = companyProfile.body.result[0]
-  const returnUrl = req.header('Referer') + '#' + companyId
 
-  return res.render('company-activity-page', { returnUrl, profile })
+  return res.render('company-activity-page', { profile })
 }
 
 module.exports = {
