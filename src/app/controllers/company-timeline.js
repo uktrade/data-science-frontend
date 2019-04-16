210af268fe55a46053d4239c025889d176c797ab
John Fieldman <hello@johnfieldman.com>
2019-04-16 11:10:11 +0100

    FEX-6 add 404 page

--- src/app/controllers/company-timeline.js
+++ src/app/controllers/company-timeline.js
@@ -1,11 +1,19 @@
+const { startCase } = require('lodash')
+
 const backendService = require('../lib/backend-service')
+const errors = require('../middleware/errors')
 
 async function renderCompanyTimeline (req, res) {
-  const companyId = req.params.company_id
-  const companyProfile = await backendService.getCompanyProfileByInternalCompanyId(companyId)
-  const profile = companyProfile.body.result[0]
+  try {
+    const companyId = req.params.company_id
+    const companyProfile = await backendService.getCompanyProfileByInternalCompanyId(companyId)
+    const profile = companyProfile.body.result[0]
+    const title = startCase(profile.company_name)
 
-  return res.render('company-activity-page', { profile })
+    return res.render('company-activity-page', { title, profile })
+  } catch (error) {
+    errors.handle404(req, res)
+  }
 }
 
 module.exports = {
