84213d72d606575578c7a7c2f24da9ffd204d131
John Fieldman <hello@johnfieldman.com>
2019-07-24 09:24:51 +0100

    added GTM script tags

--- src/app/lib/static-globals.js
+++ src/app/lib/static-globals.js
@@ -2,6 +2,7 @@ const config = require('../../../config')
 
 module.exports = function (env) {
   env.addGlobal('analyticsId', config.analyticsId)
+  env.addGlobal('googleTagManagerKey', config.googleTagManagerKey)
   env.addGlobal('feedbackLink', `${config.datahubDomain}/support`)
   env.addGlobal('headerLink', `${config.datahubDomain}/`)
   env.addGlobal('profileLink', `${config.datahubDomain}/profile`)
