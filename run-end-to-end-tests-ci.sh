d17f77ab8261ca212297bce149390915d2cd2de7
Pippo Raimondi <filipporai@gmail.com>
2019-08-02 10:24:03 +0100

    Start application before running end to end tests

--- run-end-to-end-tests-ci.sh
+++ run-end-to-end-tests-ci.sh
@@ -1,3 +1,19 @@
+export LOG_LEVEL=debug 
+
+echo "Building the assets"
+
+npm run prod
+
+echo "Starting app"
+
+`node_modules/.bin/nodemon src/app/cluster.js --ignore 'src/public/**/*' --ignore 'src/test/**/*' > app.log 2>&1` &
+
+`sleep 5`
+
+echo "App started"
+
+echo "Running tests"
+
 # Navigate to end to end folder
 cd uiTest/end-to-end
 
