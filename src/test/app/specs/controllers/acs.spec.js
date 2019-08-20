fb8799f09f62f7803a1ba78fb2e86fdc1e40d71a
Ben Pietravalle <ben@hulegu.com>
2019-08-20 11:47:49 +0100

    Fix broken tests

--- src/test/app/specs/controllers/acs.spec.js
+++ src/test/app/specs/controllers/acs.spec.js
@@ -48,7 +48,7 @@ describe('renderIndex', () => {
             }],
           },
         }))
-        renderIndex(req, res, next).then(() => {
+        return renderIndex(req, res, next).then(() => {
           expect(res.render.mock.calls.length).toBe(1)
           const renderContext = res.render.mock.calls[0][1]
           expect(renderContext.result.result[0].time_since_remove_recorded).toBe(timeSinceRemoveRecorded)
@@ -59,7 +59,7 @@ describe('renderIndex', () => {
       })
     }
     for (let garbageDate of ['', undefined, null, '2019-02-29T00:00:00', 'asdfasdf']) {
-      it(`Doesn't choke on date ${garbageDate}`, () => {
+      it(`Doesn't choke on date ${garbageDate}`, (done) => {
         repos.getData.mockReturnValueOnce(Promise.resolve({
           body: {
             result: [{
@@ -67,15 +67,146 @@ describe('renderIndex', () => {
             }],
           },
         }))
-        renderIndex(req, res, next).then(() => {
+        return renderIndex(req, res, next).then(() => {
           expect(res.render.mock.calls.length).toBe(1)
           const renderContext = res.render.mock.calls[0][1]
-          expect(renderContext.result.result[0].time_since_remove_recorded).toBe(null)
-          expect(renderContext.result.result[0].dissolved_date).toBe(null)
-          expect(renderContext.result.result[0].days_to_deletion).toBe(null)
+          expect(renderContext.result.result[0].time_since_remove_recorded).toBeFalsy()
+          expect(renderContext.result.result[0].dissolved_date).toBeFalsy()
+          expect(renderContext.result.result[0].days_to_deletion).toBeFalsy()
           done()
         })
       })
     }
+  })
+
+  describe('Preparation of GREAT integration info', () => {
+    it('Has correct URL if company is supplier and has id number', (done) => {
+      repos.getData.mockReturnValueOnce(Promise.resolve({
+        body: {
+          result: [{
+            is_published_find_a_supplier: true,
+            national_identification_number: '1',
+            national_identification_system_code: 'CRO',
+          }],
+        },
+      }))
+      return renderIndex(req, res, next).then(() => {
+        expect(res.render.mock.calls.length).toBe(1)
+        const renderContext = res.render.mock.calls[0][1]
+        expect(renderContext.result.result[0].find_a_supplier_url).toEqual(`${config.findASupplierProfileUrlPrefix}00000001/`)
+        done()
+      })
+    })
+    it('No URL if company is not supplier', (done) => {
+      repos.getData.mockReturnValueOnce(Promise.resolve({
+        body: {
+          result: [{
+            is_published_find_a_supplier: false,
+            national_identification_number: '1',
+            national_identification_system_code: config.companiesHouseIdentificationSystemCode,
+          }],
+        },
+      }))
+      return renderIndex(req, res, next).then(() => {
+        expect(res.render.mock.calls.length).toBe(1)
+        const renderContext = res.render.mock.calls[0][1]
+        expect(renderContext.result.result[0].find_a_supplier_url).toBe(null)
+        done()
+      })
+    })
+    it('No URL if company has no ID number', (done) => {
+      repos.getData.mockReturnValueOnce(Promise.resolve({
+        body: {
+          result: [{
+            is_published_find_a_supplier: true,
+            national_identification_system_code: config.companiesHouseIdentificationSystemCode,
+          }],
+        },
+      }))
+      renderIndex(req, res, next).then(() => {
+        expect(res.render.mock.calls.length).toBe(1)
+        const renderContext = res.render.mock.calls[0][1]
+        expect(renderContext.result.result[0].find_a_supplier_url).toBe(null)
+        done()
+      })
+    })
+    it('No URL if company ID number is not a Companies House number', (done) => {
+      repos.getData.mockReturnValueOnce(Promise.resolve({
+        body: {
+          result: [{
+            is_published_find_a_supplier: true,
+            national_identification_number: '2',
+            national_identification_system_code: ':Siris',
+          }],
+        },
+      }))
+      return renderIndex(req, res, next).then(() => {
+        expect(res.render.mock.calls.length).toBe(1)
+        const renderContext = res.render.mock.calls[0][1]
+        expect(renderContext.result.result[0].find_a_supplier_url).toBe(null)
+        done()
+      })
+    })
+    it('No URL if national_identification_system_code missing', (done) => {
+      repos.getData.mockReturnValueOnce(Promise.resolve({
+        body: {
+          result: [{
+            is_published_find_a_supplier: true,
+            national_identification_number: '2',
+          }],
+        },
+      }))
+      return renderIndex(req, res, next).then(() => {
+        expect(res.render.mock.calls.length).toBe(1)
+        const renderContext = res.render.mock.calls[0][1]
+        expect(renderContext.result.result[0].find_a_supplier_url).toBe(null)
+        done()
+      })
+    })
+    it('Sets is_joined_find_a_supplier to true if service_usage indicates so', (done) => {
+      repos.getData.mockReturnValueOnce(Promise.resolve({
+        body: {
+          result: [{
+            service_usage: 'asda;skfpoawkerfdit.find-a-buyer.supplierskjnawefliajnef',
+          }],
+        },
+      }))
+      return renderIndex(req, res, next).then(() => {
+        expect(res.render.mock.calls.length).toBe(1)
+        const renderContext = res.render.mock.calls[0][1]
+        expect(renderContext.result.result[0].is_joined_find_a_supplier).toBe(true)
+        done()
+      })
+    })
+    it('Sets is_joined_find_a_supplier to false if service_usage indicates so', (done) => {
+      repos.getData.mockReturnValueOnce(Promise.resolve({
+        body: {
+          result: [{
+            service_usage: 'something else',
+          }],
+        },
+      }))
+      return renderIndex(req, res, next).then(() => {
+        expect(res.render.mock.calls.length).toBe(1)
+        const renderContext = res.render.mock.calls[0][1]
+        expect(renderContext.result.result[0].is_joined_find_a_supplier).toBe(false)
+        done()
+      })
+    })
+    it('Doesnt die if service_usage not there', (done) => {
+      repos.getData.mockReturnValueOnce(Promise.resolve({
+        body: {
+          result: [{
+            national_identification_number: '1',
+          }],
+        },
+      }))
+      return renderIndex(req, res, next).then(() => {
+        expect(res.render.mock.calls.length).toBe(1)
+        const renderContext = res.render.mock.calls[0][1]
+        expect(renderContext.result.result[0].is_joined_find_a_supplier).toBe(false)
+        done()
+      })
+    })
   })
 })
