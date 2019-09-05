9cff75ddff54621ea4c01d1edd83e96a5e69c2e6
Pippo Raimondi <filipporai@gmail.com>
2019-09-05 11:38:43 +0100

    Add visual test job and refactor starting app step

---
+++ start-app.sh
@@ -0,0 +1,13 @@
+export LOG_LEVEL=debug 
+
+echo "Building the assets"
+
+npm run prod
+
+echo "Starting app"
+
+`node_modules/.bin/nodemon src/app/cluster.js --ignore 'src/public/**/*' --ignore 'src/test/**/*' > app.log 2>&1` &
+
+`sleep 5`
+
+echo "App started"
