5732c35442c69586f8cbee6e483bcdf5bae4c925
Pippo Raimondi <filipporai@gmail.com>
2019-09-11 11:39:15 +0100

    Increase timeout while waiting for app to start to prevent flaky tests

--- start-app.sh
+++ start-app.sh
@@ -8,6 +8,6 @@ echo "Starting app"
 
 `node_modules/.bin/nodemon src/app/cluster.js --ignore 'src/public/**/*' --ignore 'src/test/**/*' > app.log 2>&1` &
 
-`sleep 5`
+`sleep 20`
 
 echo "App started"
